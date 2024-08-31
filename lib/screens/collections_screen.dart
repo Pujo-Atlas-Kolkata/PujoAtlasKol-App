0f9943b4eed268369d6c358c8bbe60a96c6dc0bf
Hema <hemak83@gmail.com>
2024-08-31 15:43:13 +0530

    Added bottom navigation bar related Ui

---
+++ lib/screens/collections_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Collections extends StatelessWidget {
+  const Collections({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("Collections"));
+  }
+}
