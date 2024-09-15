f7d73402c276567e7e94e1ef958d95be9382175e
vitaminncpp <Akshaykumarparmar51@gmail.com>
2024-09-15 23:58:32 +0530

    OSM Integration in flutter.

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Map"));
+    return FlutterMap(
+      options: const MapOptions(
+          initialCenter: LatLng(22.5561697, 88.3362782),
+          initialZoom: 15,
+          interactionOptions:
+              InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom)),
+      children: [
+        openStreetMapTileLayer, // this variable needs to updates according to initial position selected from other screens
+        const MarkerLayer(markers: [ // markers need to be updated as required.
+          Marker(
+              point: LatLng(22.5561697, 88.3362782),
+              width: 60,
+              height: 60,
+              alignment: Alignment.topCenter,
+              child: Icon(Icons.location_pin, size: 60, color: Colors.red))
+        ])
+      ],
+    );
   }
 }
+
+TileLayer get openStreetMapTileLayer => TileLayer(
+      urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+      userAgentPackageName: "dev.fleaflet.flutter_map.example",
+    );
