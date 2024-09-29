ef82f3afafa77790d7257e2d88d0a2a8e2bbf48d
DIVYAKUMAR PATEL <124331485+Divyakumar21202@users.noreply.github.com>
2024-09-29 15:52:34 +0530

    Chore : created Pujo Pandal Card widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'splash.dart';
+
 import 'app.dart';
+import 'splash.dart';
 
 void main() {
   runApp(const AppView());
@@ -15,12 +16,37 @@ class AppView extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
+        textTheme: const TextTheme(
+          headlineSmall: TextStyle(
+            fontWeight: FontWeight.w400,
+            color: Color(0XFFE6E0E9),
+          ),
+          bodyMedium: TextStyle(
+            fontWeight: FontWeight.w400,
+            color: Color(
+              0xffCAC4D0,
+            ),
+          ),
+          labelLarge: TextStyle(
+            fontWeight: FontWeight.w500,
+            color: Color(
+              0xff381E72,
+            ),
+          ),
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          foregroundColor: Color(0xFFD9D9D9),
+        ),
+        scaffoldBackgroundColor: Colors.black,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       initialRoute: '/splash',
       routes: {
-        '/splash': (context) => SplashScreen(),
+        '/splash': (context) => const SplashScreen(),
         '/': (context) => const PujoAtlasApp(title: "Flutter Demo Home Page")
       },
       // home: const PujoAtlasApp(title: 'Flutter Demo Home Page'),
